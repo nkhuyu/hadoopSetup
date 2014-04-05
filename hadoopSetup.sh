97a2dfd5322ac332c7fd47000e7479625a0b2839
Yu Hu <nkhuyu@gmail.com>
2014-04-05 12:16:19 -0600

    #0: update hadoopserup

--- hadoopSetup.sh
+++ hadoopSetup.sh
@@ -3,8 +3,6 @@
 # edit /etc/hostname file
 # mkhost like hadoop0  hadoop1 hadoop2
 
-
-cd ~
 sudo apt-get update
 
 function install {
@@ -12,6 +10,19 @@ function install {
     sudo apt-get install $package_name
 }
 
-# install java
+# install java jdk
 install openjdk-7-jdk
+cd /usr/lib/jvm
+sudo ln -s java-7-openjdk-amd64 jdk
 
+# install openssh-server
+install openssh-server
+
+# add hadoop user
+sudo addgroup hadoop
+sudo adduser --ingroup hadoop hduser
+sudo adduser hduser sudo
+sudo adduser hduser admin
+
+# user doesn't need to enter password after the sudo command
+echo "hduser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoer
